b5ee7a5fd6b8d1dbbbfb6ed851bd18b474db7cfd
Kosta Harlan <kharlan@wikimedia.org>
2020-03-27 11:11:06 +0100

    Start with a sensible version number, this project does something
    Change-Id: Iad67e24dd610932513729ccdec2952a8404ed979

--- cmd/version.go
+++ cmd/version.go
@@ -25,7 +25,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Output the version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("0.0.1")
+		fmt.Println("0.1.0")
 	},
 }
 
