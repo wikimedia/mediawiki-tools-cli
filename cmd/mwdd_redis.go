365b4d79198222c61c2874712aa9247c6433698c
addshore <addshorewiki@gmail.com>
2021-04-30 08:01:05 +0100

    mwdd: Add Redis service with cli shortcut
    Change-Id: I11fd4754eb4cf5b73bfa2e62e7358f7ca7f39532

---
+++ cmd/mwdd_redis.go
@@ -0,0 +1,103 @@
+/*Package cmd is used for command line.
+
+Copyright © 2020 Addshore
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"gerrit.wikimedia.org/r/mediawiki/tools/cli/internal/exec"
+	"gerrit.wikimedia.org/r/mediawiki/tools/cli/internal/mwdd"
+	"github.com/spf13/cobra"
+)
+
+
+var mwddRedisCmd = &cobra.Command{
+	Use:   "redis",
+	Short: "Redis service",
+	RunE:  nil,
+}
+
+var mwddRedisCreateCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create a Redis container",
+	Run: func(cmd *cobra.Command, args []string) {
+		mwdd.DefaultForUser().EnsureReady()
+		mwdd.DefaultForUser().UpDetached(
+			[]string{"redis"},
+			exec.HandlerOptions{
+				Verbosity:   Verbosity,
+			},
+		)
+	},
+}
+
+var mwddRedisDestroyCmd = &cobra.Command{
+	Use:   "destroy",
+	Short: "Destroy the Redis container and volumes",
+	Run: func(cmd *cobra.Command, args []string) {
+		mwdd.DefaultForUser().EnsureReady()
+		options := exec.HandlerOptions{
+			Verbosity:   Verbosity,
+		}
+		mwdd.DefaultForUser().Rm( []string{"redis"},options)
+		mwdd.DefaultForUser().RmVolumes( []string{"redis-data"},options)
+	},
+}
+
+var mwddRedisSuspendCmd = &cobra.Command{
+	Use:   "suspend",
+	Short: "Suspend the Redis container",
+	Run: func(cmd *cobra.Command, args []string) {
+		mwdd.DefaultForUser().EnsureReady()
+		options := exec.HandlerOptions{
+			Verbosity:   Verbosity,
+		}
+		mwdd.DefaultForUser().Stop( []string{"redis"},options)
+	},
+}
+
+var mwddRedisResumeCmd = &cobra.Command{
+	Use:   "resume",
+	Short: "Resume the Redis container",
+	Run: func(cmd *cobra.Command, args []string) {
+		mwdd.DefaultForUser().EnsureReady()
+		options := exec.HandlerOptions{
+			Verbosity:   Verbosity,
+		}
+		mwdd.DefaultForUser().Start( []string{"redis"},options)
+	},
+}
+
+var mwddRedisCliCmd = &cobra.Command{
+	Use:   "cli",
+	Short: "Redis CLI for the container",
+	Run: func(cmd *cobra.Command, args []string) {
+		mwdd.DefaultForUser().EnsureReady()
+		mwdd.DefaultForUser().DockerExec(mwdd.DockerExecCommand{
+			DockerComposeService: "redis",
+			Command: []string{"redis-cli"},
+		})
+	},
+}
+
+func init() {
+	mwddCmd.AddCommand(mwddRedisCmd)
+	mwddRedisCmd.AddCommand(mwddRedisCreateCmd)
+	mwddRedisCmd.AddCommand(mwddRedisDestroyCmd)
+	mwddRedisCmd.AddCommand(mwddRedisSuspendCmd)
+	mwddRedisCmd.AddCommand(mwddRedisResumeCmd)
+	mwddRedisCmd.AddCommand(mwddRedisCliCmd)
+}
