fa5a6605ba7859c6bc8e01299d08d3a94665c2f7
addshore <addshorewiki@gmail.com>
2021-05-08 15:58:55 +0100

    mwdd: Wizard to guide you through env setup
    This includes:
     - Prompting the user to pick a PORT for the mwdd env
     - Prompting the user to pick a MW directory if not set
     - Clone of MediaWiki and optionally Vector
     - Ability to do a shallow clone for speed
     - Ability to clone from Github and switch to Gerrit
    
    This also leaves some TODOs around the place:
     - Allow the user to choose if the remotes end up on https or ssh
     - Allow cloning async for more speed
     - Allow the user to get more skins or extensions?
     - Auto detection of MW directory
     - Check if git is even installed
    
    And leaves 1 clear next step to make initial setup quite
    slick, this being do an initial composer install if
    MW determines that it needs it (will be done in a followup)
    
    Change-Id: Id58e45c26fed77a06226b081f6248d5f27a234b6

--- cmd/mwdd.go
+++ cmd/mwdd.go
@@ -18,9 +18,14 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package cmd
 
 import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+
 	"gerrit.wikimedia.org/r/mediawiki/tools/cli/internal/exec"
 	"gerrit.wikimedia.org/r/mediawiki/tools/cli/internal/mwdd"
-	"fmt"
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
 
@@ -28,6 +33,33 @@ var mwddCmd = &cobra.Command{
 	Use:   "mwdd",
 	Short: "The MediaWiki-Docker-Dev like development environment",
 	RunE:  nil,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		mwdd := mwdd.DefaultForUser()
+		mwdd.EnsureReady()
+		if(mwdd.Env().Missing("PORT")){
+			prompt := promptui.Prompt{
+				Label:     "What port would you like to use for your development environment?",
+				// TODO suggest a port that is defiantly availbile for listening on
+				Default: "8080",
+				Validate: func(input string) error {
+					// TODO check the port can be listened on?
+					// https://coolaj86.com/articles/how-to-test-if-a-port-is-available-in-go/
+					_, err := strconv.ParseFloat(input, 64)
+					if err != nil {
+						return errors.New("Invalid number")
+					}
+					return nil
+				},
+			}
+			value, err := prompt.Run()
+			if err == nil {
+				mwdd.Env().Set("PORT",value)
+			} else {
+				fmt.Println("Can't continue without a port")
+				os.Exit(1)
+			}
+		}
+	},
 }
 
 var mwddWhereCmd = &cobra.Command{
@@ -42,7 +74,6 @@ var mwddCreateCmd = &cobra.Command{
 	Use:   "create",
 	Short: "Create the Default containers",
 	Run: func(cmd *cobra.Command, args []string) {
-		mwdd.DefaultForUser().EnsureReady()
 		options := exec.HandlerOptions{
 			Verbosity:   Verbosity,
 		}
@@ -57,7 +88,6 @@ var mwddDestroyCmd = &cobra.Command{
 	Use:   "destroy",
 	Short: "Destroy the Default containers",
 	Run: func(cmd *cobra.Command, args []string) {
-		mwdd.DefaultForUser().EnsureReady()
 		options := exec.HandlerOptions{
 			Verbosity:   Verbosity,
 		}
