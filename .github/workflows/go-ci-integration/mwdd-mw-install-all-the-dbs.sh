78fb29e48da654ec9490e2e170bfb88d263e4316
addshore <addshorewiki@gmail.com>
2021-05-08 21:04:43 +0100

    GHA: tests for postgres, adminer, phpmyadmin & fixes
    Change-Id: I4dbf8919943e35c8d5b3d213a8df6073cc61d49f

---
+++ .github/workflows/go-ci-integration/mwdd-mw-install-all-the-dbs.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Fail on errors
+set -e
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+# Output commands
+set -x
+
+# Setup & Create
+./mw mwdd env set PORT 8080
+./mw mwdd env set MEDIAWIKI_VOLUMES_CODE $(pwd)/mediawiki
+./mw mwdd create
+
+# Validate the basic stuff
+./mw mwdd docker-compose ps
+./mw mwdd env list
+cat ~/.mwcli/mwdd/default/.env
+curl -s -L -N http://default.mediawiki.mwdd.localhost:8080 | grep -q "The MediaWiki logo"
+
+# Add the needed LocalSettings
+echo "<?php" >> mediawiki/LocalSettings.php
+echo "//require_once "$IP/includes/PlatformSettings.php";" >> mediawiki/LocalSettings.php
+echo "require_once '/mwdd/MwddSettings.php';" >> mediawiki/LocalSettings.php
+
+# Install sqlite & check
+./mw mwdd mediawiki install
+curl -s -L -N http://default.mediawiki.mwdd.localhost:8080 | grep -q "MediaWiki has been installed"
+
+# Turn on mysql, install & check
+./mw mwdd mysql create
+./mw mwdd mediawiki install --dbname mysqlwiki --dbtype mysql
+curl -s -L -N http://mysqlwiki.mediawiki.mwdd.localhost:8080 | grep -q "MediaWiki has been installed"
+
+# Turn on postgres, install & check
+./mw mwdd postgres create
+./mw mwdd mediawiki install --dbname postgreswiki --dbtype postgres
+curl -s -L -N http://postgreswiki.mediawiki.mwdd.localhost:8080 | grep -q "MediaWiki has been installed"
+
+# Turn on the db management services
+./mw mwdd phpmyadmin create
+./mw mwdd adminer create
+sleep 2
+curl -s -L -N http://phpmyadmin.mwdd.localhost:8080 | grep -q "Open new phpMyAdmin window"
+curl -s -L -N http://adminer.mwdd.localhost:8080 | grep -q "Login - Adminer"
+
+# Make sure the expected number of services appear
+docker ps
+docker ps | wc -l | grep -q "8"
+
+# Destroy it all
+./mw mwdd destroy
+# And make sure only 1 exists after
+docker ps
+docker ps | wc -l | grep -q "1"
--- .github/workflows/go-ci-integration/mwdd-mysql-suspend-resume-destroy.sh
+++ .github/workflows/go-ci-integration/mwdd-mysql-suspend-resume-destroy.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+# Fail on errors
+set -e
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+# Output commands
+set -x
+
 # Setup & Create
+./mw mwdd env set PORT 8080
 ./mw mwdd env set MEDIAWIKI_VOLUMES_CODE $(pwd)/mediawiki
 ./mw mwdd create
 ./mw mwdd mysql create
@@ -15,19 +25,21 @@ cat ~/.mwcli/mwdd/default/.env
 
 # Install & check
 ./mw mwdd mediawiki install --dbname mysqlwiki --dbtype mysql
-curl -s -L http://mysqlwiki.mediawiki.mwdd.localhost:8080 | grep -q "MediaWiki has been installed"
+curl -s -L -N http://mysqlwiki.mediawiki.mwdd.localhost:8080 | grep -q "MediaWiki has been installed"
 
 # Suspend and resume and check the site is still there
 ./mw mwdd mysql suspend
 ./mw mwdd mysql resume
-curl -s -L http://mysqlwiki.mediawiki.mwdd.localhost:8080 | grep -q "MediaWiki has been installed"
+sleep 2
+curl -s -L -N http://mysqlwiki.mediawiki.mwdd.localhost:8080 | grep -q "MediaWiki has been installed"
 
 # Destroy and restart mysql, reinstalling mediawiki
 ./mw mwdd mysql destroy
 ./mw mwdd mysql create
 ./mw mwdd mediawiki install --dbname mysqlwiki --dbtype mysql
-curl -s -L http://mysqlwiki.mediawiki.mwdd.localhost:8080 | grep -q "MediaWiki has been installed"
+curl -s -L -N http://mysqlwiki.mediawiki.mwdd.localhost:8080 | grep -q "MediaWiki has been installed"
 
 # Destroy it all
 ./mw mwdd destroy
+docker ps
 docker ps | wc -l | grep -q "1"
