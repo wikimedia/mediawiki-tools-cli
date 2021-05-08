4265bf9a43450842dc9a2c6ef44af7ecfbcbd1e3
addshore <addshorewiki@gmail.com>
2021-05-08 11:40:50 +0100

    More lint fixes
    Change-Id: I8402e9700b5d57c9355e6c2f595a2c9ef9eb28f1

--- internal/mwdd/docker.go
+++ internal/mwdd/docker.go
@@ -39,15 +39,14 @@ type DockerExecCommand struct {
 	HandlerOptions exec.HandlerOptions
 }
 
-/* UserAndGroupForDockerExecution gets a user and group id combination for the current user that can be used for execution*/
+/*UserAndGroupForDockerExecution gets a user and group id combination for the current user that can be used for execution*/
 func UserAndGroupForDockerExecution() string {
 	if(runtime.GOOS == "windows") {
 		// TODO confirm that just using 2000 will always work on Windows?
 		// This user won't exist, but that fact doesn't really matter on pure Windows
 		return "2000:2000"
-	} else {
-		return fmt.Sprint(os.Getuid(), ":", os.Getgid())
 	}
+	return fmt.Sprint(os.Getuid(), ":", os.Getgid())
 }
 
 /*DockerExec runs a docker exec command using the docker SDK*/
