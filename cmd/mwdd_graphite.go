368fd8feb40e9cbdd7dcca7f9fc50fda1ef7eae8
addshore <addshorewiki@gmail.com>
2021-05-09 21:53:08 +0100

    mwdd: Add graphite-statsd
    Bug: T282333
    Change-Id: Ie6211e27211316e84da045f91f4afee3929c9594

---
+++ cmd/mwdd_graphite.go
@@ -0,0 +1,90 @@
+/*Package cmd is used for command line.
+
+Copyright © 2020 Addshore
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"gerrit.wikimedia.org/r/mediawiki/tools/cli/internal/exec"
+	"gerrit.wikimedia.org/r/mediawiki/tools/cli/internal/mwdd"
+	"github.com/spf13/cobra"
+)
+
+
+var mwddGraphiteCmd = &cobra.Command{
+	Use:   "graphite",
+	Short: "Graphite service",
+	RunE:  nil,
+}
+
+var mwddGraphiteCreateCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create the Graphite containers",
+	Run: func(cmd *cobra.Command, args []string) {
+		mwdd.DefaultForUser().EnsureReady()
+		mwdd.DefaultForUser().UpDetached(
+			[]string{"graphite"},
+			exec.HandlerOptions{
+				Verbosity:   Verbosity,
+			},
+		)
+	},
+}
+
+var mwddGraphiteDestroyCmd = &cobra.Command{
+	Use:   "destroy",
+	Short: "Destroy the Graphite containers and volumes",
+	Run: func(cmd *cobra.Command, args []string) {
+		mwdd.DefaultForUser().EnsureReady()
+		options := exec.HandlerOptions{
+			Verbosity:   Verbosity,
+		}
+		mwdd.DefaultForUser().Rm( []string{"graphite"},options)
+		mwdd.DefaultForUser().RmVolumes( []string{"graphite-storage","graphite-logs"},options)
+	},
+}
+
+var mwddGraphiteSuspendCmd = &cobra.Command{
+	Use:   "suspend",
+	Short: "Suspend the Graphite containers",
+	Run: func(cmd *cobra.Command, args []string) {
+		mwdd.DefaultForUser().EnsureReady()
+		options := exec.HandlerOptions{
+			Verbosity:   Verbosity,
+		}
+		mwdd.DefaultForUser().Stop( []string{"graphite"},options)
+	},
+}
+
+var mwddGraphiteResumeCmd = &cobra.Command{
+	Use:   "resume",
+	Short: "Resume the Graphite containers",
+	Run: func(cmd *cobra.Command, args []string) {
+		mwdd.DefaultForUser().EnsureReady()
+		options := exec.HandlerOptions{
+			Verbosity:   Verbosity,
+		}
+		mwdd.DefaultForUser().Start( []string{"graphite"},options)
+	},
+}
+
+func init() {
+	mwddCmd.AddCommand(mwddGraphiteCmd)
+	mwddGraphiteCmd.AddCommand(mwddGraphiteCreateCmd)
+	mwddGraphiteCmd.AddCommand(mwddGraphiteDestroyCmd)
+	mwddGraphiteCmd.AddCommand(mwddGraphiteSuspendCmd)
+	mwddGraphiteCmd.AddCommand(mwddGraphiteResumeCmd)
+}
