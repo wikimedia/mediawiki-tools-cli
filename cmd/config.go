612b75286199d53782264bdd156743bde6200d20
addshore <addshorewiki@gmail.com>
2021-07-08 13:37:29 +0100

    Introduce a cli config and config command
    This introduces a basic JSON config that can be expanded and
    used initially for the "dev mode" option, of either mediawiki-docker
    or mwdd.
    
    Bug: T286088
    Change-Id: I5aa878737e7ac42ffa5b1abdb1f867db1c539c91

---
+++ cmd/config.go
@@ -0,0 +1,42 @@
+/*Package cmd is used for command line.
+
+Copyright © 2020 Addshore
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"gerrit.wikimedia.org/r/mediawiki/tools/cli/internal/config"
+	"github.com/spf13/cobra"
+)
+
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Display or change configuration settings",
+}
+
+var configShowCmd = &cobra.Command{
+    Use:   "show",
+    Short: "Shows the raw config",
+    Run: func(cmd *cobra.Command, args []string) {
+        fmt.Println(config.LoadFromDisk())
+    },
+}
+
+func init() {
+	rootCmd.AddCommand(configCmd)
+    configCmd.AddCommand(configShowCmd)
+}
