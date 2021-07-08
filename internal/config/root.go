f63a1329df69bbe809c7e6ef34bf0268b3031199
addshore <addshorewiki@gmail.com>
2021-07-08 13:37:29 +0100

    Introduce a cli config and config command
    This introduces a basic JSON config that can be expanded and
    used initially for the "dev mode" option, of either mediawiki-docker
    or mwdd.
    
    Bug: T286088
    Change-Id: I5aa878737e7ac42ffa5b1abdb1f867db1c539c91

---
+++ internal/config/root.go
@@ -0,0 +1,89 @@
+/*Package config for interacting with the cli config
+
+Copyright © 2020 Addshore
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package config
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/user"
+	"strings"
+)
+
+/*Config representation of a cli config*/
+type Config struct {
+}
+
+func configPath() string {
+	return mwcliDirectory() + string(os.PathSeparator) + "config.json"
+}
+
+func mwcliDirectory() string {
+	currentUser, _ := user.Current()
+	projectDirectory := currentUser.HomeDir + string(os.PathSeparator) + ".mwcli"
+	return projectDirectory
+}
+
+func ensureExists() {
+	if _, err := os.Stat(configPath()); err != nil {
+		err := os.MkdirAll(strings.Replace(configPath(), "config.json", "", -1), 0700)
+		if err != nil {
+			log.Fatal(err)
+		}
+		file, err := os.OpenFile(configPath(), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+		w := bufio.NewWriter(file)
+		_, err = w.WriteString("{}")
+		if err != nil {
+			log.Fatal(err)
+		}
+		w.Flush()
+	}
+}
+
+/*LoadFromDisk loads the config.json from disk*/
+func LoadFromDisk() (Config) {
+	ensureExists()
+    var config Config
+    configFile, err := os.Open(configPath())
+    defer configFile.Close()
+    if err != nil {
+        fmt.Println(err.Error())
+    }
+    jsonParser := json.NewDecoder(configFile)
+    jsonParser.Decode(&config)
+    return config
+}
+
+/*WriteToDisk writers the config to disk*/
+func (c Config) WriteToDisk() {
+	file, err := os.OpenFile(configPath(), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	w := bufio.NewWriter(file)
+	jsonEncoder := json.NewEncoder(w)
+	jsonEncoder.Encode(c)
+	w.Flush()
+}
