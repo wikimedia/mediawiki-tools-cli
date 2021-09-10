9bc7baa22779fa63d75cf627576d59993e0c24df
addshore <addshorewiki@gmail.com>
2021-09-10 10:22:01 +0100

    dev: Resize terminal on exec commands

--- internal/mwdd/docker.go
+++ internal/mwdd/docker.go
@@ -22,12 +22,14 @@ import (
 	"fmt"
 	"io"
 	"os"
+	gosignal "os/signal"
 	"runtime"
 	"time"
 
 	"gerrit.wikimedia.org/r/mediawiki/tools/cli/internal/exec"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/signal"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
@@ -60,7 +62,7 @@ func (m MWDD) DockerExec(command DockerExecCommand) {
 		panic(err)
 	}
 
-	config := types.ExecConfig{
+	execConfig := types.ExecConfig{
 		AttachStderr: true,
 		AttachStdout: true,
 		AttachStdin:  true,
@@ -71,7 +73,7 @@ func (m MWDD) DockerExec(command DockerExecCommand) {
 	}
 
 	ctx := context.Background()
-	response, err := cli.ContainerExecCreate(ctx, containerID, config)
+	response, err := cli.ContainerExecCreate(ctx, containerID, execConfig)
 	if err != nil {
 		return
 	}
@@ -90,6 +92,13 @@ func (m MWDD) DockerExec(command DockerExecCommand) {
 	if err != nil {
 		fmt.Println(err)
 		return
+	}
+
+	if execConfig.Tty {
+		if err := monitorTtySize(ctx, cli, execID, true); err != nil {
+			fmt.Println("Error monitoring TTY size:")
+			fmt.Println(err)
+		}
 	}
 
 	// When TTY is ON, just copy stdout https://phabricator.wikimedia.org/T282340
@@ -118,4 +127,50 @@ func (m MWDD) DockerExec(command DockerExecCommand) {
 			break
 		}
 	}
+}
+
+// MonitorTtySize updates the container tty size when the terminal tty changes size
+func monitorTtySize(ctx context.Context, client client.APIClient, id string, isExec bool) error {
+	// Source: https://github.com/skiffos/skiff-core/blob/82c430e4961453c250883c2e5ebd4bd360fa13a5/shell/tty.go
+	resizeTty := func() {
+		width, height, _ := terminal.GetSize(0)
+		resizeTtyTo(ctx, client, id, uint(height), uint(width), isExec)
+	}
+
+	resizeTty()
+
+	sigchan := make(chan os.Signal, 1)
+	gosignal.Notify(sigchan, signal.SIGWINCH)
+	go func() {
+		for range sigchan {
+			resizeTty()
+		}
+	}()
+
+	return nil
+}
+
+func resizeTtyTo(ctx context.Context, client client.ContainerAPIClient, id string, height, width uint, isExec bool) {
+	// Source: https://github.com/skiffos/skiff-core/blob/82c430e4961453c250883c2e5ebd4bd360fa13a5/shell/tty.go
+	if height == 0 && width == 0 {
+		return
+	}
+
+	options := types.ResizeOptions{
+		Height: height,
+		Width:  width,
+	}
+
+	var err error
+	if isExec {
+		err = client.ContainerExecResize(ctx, id, options)
+	} else {
+		err = client.ContainerResize(ctx, id, options)
+	}
+
+	_ = err // Ignore this error for now.
+	/*
+		if err != nil {
+		}
+	*/
 }
