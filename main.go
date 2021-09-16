a0d725efd30b9972b00926902ee91de6675326e6
addshore <addshorewiki@gmail.com>
2021-09-16 21:44:53 +0100

    Run gofmt -s, as Go report card wants it

--- main.go
+++ main.go
@@ -22,14 +22,19 @@ import "gerrit.wikimedia.org/r/mediawiki/tools/cli/cmd"
 
 /*GitCommit holds short commit hash of source tree*/
 var GitCommit string
+
 /*GitBranch holds current branch name the code is built off*/
 var GitBranch string
+
 /*GitState shows whether there are uncommitted changes*/
 var GitState string
+
 /*GitSummary holds output of git describe --tags --dirty --always*/
 var GitSummary string
+
 /*BuildDate holds RFC3339 formatted UTC date (build time)*/
 var BuildDate string
+
 /*Version holds contents of ./VERSION file, if exists, or the value passed via the -version option*/
 var Version string
 
