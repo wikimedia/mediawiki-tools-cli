8557a3a7d1d792feb7f629c53fba10162e343203
Ollie Shotton <oliver.shotton@wikimedia.de>
2022-08-21 14:03:48 +0000

    Add JUnit report to `test` CI job

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -35,6 +35,12 @@ $(GOLINT): $(BINGO_DIR)/golint.mod
 	@echo "(re)installing $(GOBIN)/golint-v0.0.0-20210508222113-6edffad5e616"
 	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=golint.mod -o=$(GOBIN)/golint-v0.0.0-20210508222113-6edffad5e616 "golang.org/x/lint/golint"
 
+GOTESTSUM := $(GOBIN)/gotestsum-v1.8.2
+$(GOTESTSUM): $(BINGO_DIR)/gotestsum.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/gotestsum-v1.8.2"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=gotestsum.mod -o=$(GOBIN)/gotestsum-v1.8.2 "gotest.tools/gotestsum"
+
 GOVVV := $(GOBIN)/govvv-v0.3.0
 $(GOVVV): $(BINGO_DIR)/govvv.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
