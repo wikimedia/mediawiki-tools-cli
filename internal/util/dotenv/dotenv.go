62b4db5e6473b8eb778ba2da8e23506854b0d6eb
Ollie Shotton <oliver.shotton@wikimedia.de>
2022-08-24 20:12:06 +0100

    DNM: Test CI code coverage

--- internal/util/dotenv/dotenv.go
+++ internal/util/dotenv/dotenv.go
@@ -14,21 +14,25 @@ type File string
 
 /*FileForDirectory returns the File for the given directory.*/
 func FileForDirectory(directory string) File {
+
 	return File(directory + string(os.PathSeparator) + ".env")
 }
 
 /*Path the path of the .env file.*/
 func (f File) Path() string {
+
 	return string(f)
 }
 
 /*EnsureExists ensures that the File exists, creating an empty one if not.*/
 func (f File) EnsureExists() {
+
 	if _, err := os.Stat(f.Path()); err != nil {
 		err := sudoaware.MkdirAll(strings.Replace(f.Path(), filepath.Base(f.Path()), "", -1), 0o700)
 		if err != nil {
 			panic(err)
 		}
+
 		_, err = sudoaware.OpenFile(f.Path(), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0o600)
 		if err != nil {
 			panic(err)
@@ -37,52 +41,64 @@ func (f File) EnsureExists() {
 }
 
 func (f File) read() map[string]string {
+
 	f.EnsureExists()
 	envMap, _ := godotenv.Read(f.Path())
+
 	return envMap
 }
 
 func (f File) write(envMap map[string]string) {
+
 	// Override the regular gotdotenv Write method to avoid adding quotes
 	// https://github.com/joho/godotenv/issues/50#issuecomment-364873528
 	// https://github.com/moby/moby/issues/12997
 	// godotenv.Write(envMap, f.Path())
 	writeOverride(envMap, f.Path())
+
 }
 
 /*Delete a value from the env.*/
 func (f File) Delete(name string) {
+
 	envMap := f.read()
 	delete(envMap, name)
+
 	f.write(envMap)
 }
 
 /*Set a value in the env.*/
 func (f File) Set(name string, value string) {
+
 	envMap := f.read()
 	envMap[name] = value
+
 	f.write(envMap)
 }
 
 /*Get a value from the env.*/
 func (f File) Get(name string) string {
 	envMap := f.read()
+
 	return envMap[name]
 }
 
 /*Has a value in the env.*/
 func (f File) Has(name string) bool {
 	envMap := f.read()
+
 	_, ok := envMap[name]
 	return ok
 }
 
 /*Missing a value in the env.*/
 func (f File) Missing(name string) bool {
+
 	return !f.Has(name)
 }
 
 /*List all values from the env.*/
 func (f File) List() map[string]string {
+
 	return f.read()
 }
