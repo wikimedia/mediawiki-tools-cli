b38a55440c5b097b2e600730b685df2bdaff4f19
addshore <addshorewiki@gmail.com>
2022-10-06 22:23:14 +0100

    Support fancy outputs in version command

--- internal/cmd/version/root.go
+++ internal/cmd/version/root.go
@@ -6,31 +6,51 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"gitlab.wikimedia.org/repos/releng/cli/internal/cli"
+	"gitlab.wikimedia.org/repos/releng/cli/internal/util/output"
 )
 
 func NewVersionCmd() *cobra.Command {
+	out := versionOutput()
 	cmd := &cobra.Command{
 		Use:   "version",
 		Short: "Output the version information",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("mw version", cli.VersionDetails.Version, "(", cli.VersionDetails.BuildDate, ")")
-			fmt.Println("https://gitlab.wikimedia.org/repos/releng/cli/-/releases")
+			objects := make(map[interface{}]interface{}, 7)
 
-			logrus.Debugf(`GitCommit: %s
-			GitBranch: %s
-			GitState: %s
-			GitSummary: %s
-			BuildDate: %s
-			Version: %s
-			`,
-				cli.VersionDetails.GitCommit,
-				cli.VersionDetails.GitBranch,
-				cli.VersionDetails.GitState,
-				cli.VersionDetails.GitSummary,
-				cli.VersionDetails.BuildDate,
-				cli.VersionDetails.Version,
-			)
+			if logrus.GetLevel() > logrus.InfoLevel {
+				objects["GitCommit"] = cli.VersionDetails.GitCommit
+				objects["GitBranch"] = cli.VersionDetails.GitBranch
+				objects["GitState"] = cli.VersionDetails.GitState
+				objects["GitSummary"] = cli.VersionDetails.GitSummary
+			}
+
+			objects["BuildDate"] = cli.VersionDetails.BuildDate
+			objects["Version"] = cli.VersionDetails.Version
+			objects["Releases"] = "https://gitlab.wikimedia.org/repos/releng/cli/-/releases"
+
+			out.Print(objects)
 		},
 	}
+
+	out.AddFlags(cmd, "table")
 	return cmd
+}
+
+func versionOutput() output.Output {
+	return output.Output{
+		TopLevelKeys: true,
+		TableBinding: &output.TableBinding{
+			Headings: []string{"Version Infomation", "Value"},
+			ProcessObjects: func(objects map[interface{}]interface{}, table *output.Table) {
+				for key, value := range objects {
+					table.AddRowS(fmt.Sprintf("%s", key), fmt.Sprintf("%s", value))
+				}
+			},
+		},
+		AckBinding: func(objects map[interface{}]interface{}, ack *output.Ack) {
+			for key, value := range objects {
+				ack.AddItem("Version Infomation", fmt.Sprintf("%s: %s", key, value))
+			}
+		},
+	}
 }
