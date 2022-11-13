a0c757aca36f6a586edaf0dbad162bad2dc03358
addshore <addshorewiki@gmail.com>
2022-11-13 11:04:59 -0100

    Split some files up

---
+++ internal/cmd/config/show.go
@@ -0,0 +1,16 @@
+package config
+
+import (
+	"github.com/spf13/cobra"
+	"gitlab.wikimedia.org/repos/releng/cli/internal/config"
+)
+
+func NewConfigShowCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "show",
+		Short: "Shows the raw config",
+		Run: func(cmd *cobra.Command, args []string) {
+			config.LoadFromDisk().PrettyPrint()
+		},
+	}
+}
---
+++ internal/cmd/config/where.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"gitlab.wikimedia.org/repos/releng/cli/internal/config"
+)
+
+func NewConfigWhereCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "where",
+		Short: "Outputs the path to the config file",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(config.Path())
+		},
+	}
+}
