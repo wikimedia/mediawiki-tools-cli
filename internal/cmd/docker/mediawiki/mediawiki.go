537749a183acc3069d471e8f65be47ac4d6d31e3
Addshore <addshorewiki@gmail.com>
2023-02-11 14:15:06 +0000

    Add docker mediawiki get-code command

--- internal/cmd/docker/mediawiki/mediawiki.go
+++ internal/cmd/docker/mediawiki/mediawiki.go
@@ -10,7 +10,6 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"gitlab.wikimedia.org/repos/releng/cli/internal/cli"
-	"gitlab.wikimedia.org/repos/releng/cli/internal/eventlogging"
 	"gitlab.wikimedia.org/repos/releng/cli/internal/mediawiki"
 	"gitlab.wikimedia.org/repos/releng/cli/internal/mwdd"
 	cobrautil "gitlab.wikimedia.org/repos/releng/cli/internal/util/cobra"
@@ -80,134 +79,11 @@ func NewMediaWikiCmd() *cobra.Command {
 				mwdd.Env().Set("MEDIAWIKI_VOLUMES_DOT_COMPOSER", usrDir+"/.composer")
 			}
 
-			setupOpts := mediawiki.CloneSetupOpts{}
 			mediawiki, _ := mediawiki.ForDirectory(mwdd.Env().Get("MEDIAWIKI_VOLUMES_CODE"))
-
-			// TODO ask a question about what remotes you want to end up using? https vs ssh!
-			// TODO ask if they want to get any more skins and extensions?
-			// TODO async cloning of repos for speed!
-			if !mediawiki.MediaWikiIsPresent() {
-				logrus.Debug("MediaWiki is missing")
-				if !cli.Opts.NoInteraction {
-					cloneMw := false
-					prompt := &survey.Confirm{
-						Message: "MediaWiki code not detected in " + mwdd.Env().Get("MEDIAWIKI_VOLUMES_CODE") + ". Do you want to clone it now? (Negative answers will abort this command)",
-					}
-					err := survey.AskOne(prompt, &cloneMw)
-					if err != nil {
-						fmt.Println(err)
-						os.Exit(1)
-					}
-					setupOpts.GetMediaWiki = cloneMw
-				} else {
-					setupOpts.GetMediaWiki = true
-				}
-			}
-			if !mediawiki.VectorIsPresent() {
-				logrus.Debug("Vector is missing")
-				if !cli.Opts.NoInteraction {
-					cloneVector := false
-					prompt := &survey.Confirm{
-						Message: "Vector skin is not detected in " + mwdd.Env().Get("MEDIAWIKI_VOLUMES_CODE") + ". Do you want to clone it now?",
-					}
-					err := survey.AskOne(prompt, &cloneVector)
-					if err != nil {
-						fmt.Println(err)
-						os.Exit(1)
-					}
-					setupOpts.GetVector = cloneVector
-				} else {
-					setupOpts.GetVector = true
-				}
-			}
-			if setupOpts.GetMediaWiki || setupOpts.GetVector {
-				if !cli.Opts.NoInteraction {
-					cloneFromGithub := false
-					prompt1 := &survey.Confirm{
-						Message: "Do you want to clone from Github for extra speed? (your git remotes will be switched to Gerrit after download)",
-					}
-					err := survey.AskOne(prompt1, &cloneFromGithub)
-					if err != nil {
-						fmt.Println(err)
-						os.Exit(1)
-					}
-					setupOpts.UseGithub = cloneFromGithub
-
-					cloneShallow := false
-					prompt2 := &survey.Confirm{
-						Message: "Do you want to use shallow clones for extra speed? (You can fetch all history later using `git fetch --unshallow`)",
-					}
-					err = survey.AskOne(prompt2, &cloneShallow)
-					if err != nil {
-						fmt.Println(err)
-						os.Exit(1)
-					}
-					setupOpts.UseShallow = cloneFromGithub
-
-					finalRemoteType := ""
-					prompt3 := &survey.Select{
-						Message: "How do you want to interact with Gerrit for the cloned repositores?",
-						Options: []string{"ssh", "http"},
-						Default: "ssh",
-					}
-					err = survey.AskOne(prompt3, &finalRemoteType)
-					if err != nil {
-						fmt.Println(err)
-						os.Exit(1)
-					}
-					setupOpts.GerritInteractionType = finalRemoteType
-
-					if finalRemoteType == "ssh" {
-						gerritUsername := ""
-						prompt := &survey.Input{
-							Message: "What is your Gerrit username?",
-						}
-						err = survey.AskOne(prompt, &gerritUsername)
-						if err != nil {
-							fmt.Println(err)
-							os.Exit(1)
-						}
-
-						if len(gerritUsername) < 1 {
-							fmt.Println("Gerrit username required for ssh interaction type.")
-							os.Exit(1)
-						}
-						setupOpts.GerritUsername = gerritUsername
-					}
-				} else {
-					setupOpts.UseGithub = true
-					setupOpts.UseShallow = true
-					// Default is ssh, but http is the only non interactive choice we can make here..
-					setupOpts.GerritInteractionType = "http"
-				}
-			}
-
-			if setupOpts.GetMediaWiki || setupOpts.GetVector {
-				// Clone various things in multiple stages
-
-				// TODO add a spinner back here
-				// We removed it while untangling a big old mess
-				fmt.Println("Cloning repositories...")
-				fmt.Println("This may take a few moments...")
-				mediawiki.CloneSetup(setupOpts)
-
-				eventlogging.AddFeatureUsageEvent("clone-repositories", cli.VersionDetails.Version)
-				if setupOpts.UseGithub {
-					eventlogging.AddFeatureUsageEvent("clone-repositories:use-github", cli.VersionDetails.Version)
-				}
-				if setupOpts.UseShallow {
-					eventlogging.AddFeatureUsageEvent("clone-repositories:use-shallow", cli.VersionDetails.Version)
-				}
-
-				// Check that the needed things seem to have happened
-				if setupOpts.GetMediaWiki && !mediawiki.MediaWikiIsPresent() {
-					fmt.Println("Something went wrong cloning MediaWiki")
-					os.Exit(1)
-				}
-				if setupOpts.GetVector && !mediawiki.VectorIsPresent() {
-					fmt.Println("Something went wrong cloning Vector")
-					os.Exit(1)
-				}
+			if !mediawiki.MediaWikiIsPresent() || !mediawiki.VectorIsPresent() {
+				fmt.Println("MediaWiki or Vector is not present in the code directory")
+				fmt.Println("You can clone them manually or use `mw docker mediawiki get-code")
+				os.Exit(1)
 			}
 		},
 	}
@@ -226,6 +102,7 @@ func NewMediaWikiCmd() *cobra.Command {
 	cmd.AddCommand(mwdd.NewServiceStopCmd("mediawiki"))
 	cmd.AddCommand(mwdd.NewServiceStartCmd("mediawiki"))
 	cmd.AddCommand(NewMediaWikiInstallCmd())
+	cmd.AddCommand(NewMediaWikiGetCodeCmd())
 	cmd.AddCommand(NewMediaWikiComposerCmd())
 	cmd.AddCommand(NewMediaWikiExecCmd())
 	cmd.AddCommand(NewMediaWikiSitesCmd())
