989cc4099af3eee0dadef4c396ee6affa1e48982
Marius Hoch <mail@mariushoch.de>
2024-05-04 22:37:40 +0300

    tests: Improve argument quoting
    ... and make shellcheck happy (for tests/functions.sh,
    at least).

--- tests/test-general.sh
+++ tests/test-general.sh
@@ -7,25 +7,25 @@ source $SCRIPT_DIR/functions.sh
 
 export MWCLI_CONTEXT_TEST=1
 
-test_command_success "./bin/mw version"
+test_command_success ./bin/mw version
 
-test_command_success "./bin/mw debug"
-test_command_success "./bin/mw debug events"
-test_command_success "./bin/mw debug events cat"
-test_command_success "./bin/mw debug events submit"
+test_command_success ./bin/mw debug
+test_command_success ./bin/mw debug events
+test_command_success ./bin/mw debug events cat
+test_command_success ./bin/mw debug events submit
 
-test_command_success "./bin/mw config"
-test_command_success "./bin/mw config show"
-test_command_success "./bin/mw config where"
+test_command_success ./bin/mw config
+test_command_success ./bin/mw config show
+test_command_success ./bin/mw config where
 # Roundtrip setting a single value
 PREVIOUS_TELEMETRY_VAL=$(./bin/mw config get telemetry)
-test_command_success "./bin/mw config set telemetry foo"
-test_command "./bin/mw config get telemetry" "foo"
+test_command_success ./bin/mw config set telemetry foo
+test_command ./bin/mw config get telemetry "foo"
 ./bin/mw config set telemetry $PREVIOUS_TELEMETRY_VAL
 
 # Help topics...
-test_command_success "./bin/mw output"
+test_command_success ./bin/mw output
 
 # gitlab: Test command is registered and generally works
-test_command_success "./bin/mw gitlab"
-test_command_success "./bin/mw gitlab alias list"
+test_command_success ./bin/mw gitlab
+test_command_success ./bin/mw gitlab alias list
