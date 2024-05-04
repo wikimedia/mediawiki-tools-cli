cd3653e6baf84558a8f028c1d4af0a4751effb9d
addshore <addshorewiki@gmail.com>
2024-05-04 12:03:39 +0300

    Allow updating to main gitlab builds

--- internal/updater/root.go
+++ internal/updater/root.go
@@ -1,6 +1,16 @@
 package updater
 
-import "github.com/sirupsen/logrus"
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
 
 /*CanUpdate will check for updates.*/
 func CanUpdate(currentVersion string, gitSummary string) (bool, string) {
@@ -25,4 +35,98 @@ func CanMoveToVersion(targetVersion string) bool {
 
 func MoveToVersion(targetVersion string) (success bool, message string) {
 	return MoveToVersionFromGitlab(targetVersion)
+}
+
+func DownloadFile(fullURLFile string) (string, error) {
+	// Create blank file in a temporary location
+	file, err := os.CreateTemp("", "mwcli-tempfile")
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	client := http.Client{
+		CheckRedirect: func(r *http.Request, via []*http.Request) error {
+			r.URL.Opaque = r.URL.Path
+			return nil
+		},
+	}
+
+	// Put content on file
+	resp, err := client.Get(fullURLFile)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	defer file.Close()
+
+	return file.Name(), nil
+}
+
+func Unzip(src, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := r.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	os.MkdirAll(dest, 0755)
+
+	// Closure to address file descriptors issue with all the deferred .Close() methods
+	extractAndWriteFile := func(f *zip.File) error {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := rc.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		path := filepath.Join(dest, f.Name)
+
+		// Check for ZipSlip (Directory traversal)
+		if !strings.HasPrefix(path, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return fmt.Errorf("illegal file path: %s", path)
+		}
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			os.MkdirAll(filepath.Dir(path), f.Mode())
+			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer func() {
+				if err := f.Close(); err != nil {
+					panic(err)
+				}
+			}()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	for _, f := range r.File {
+		err := extractAndWriteFile(f)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
