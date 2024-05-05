752aa354e247901388f34263998769136c5e83c9
addshore <addshorewiki@gmail.com>
2024-05-05 14:33:40 +0300

    Test with --gerrit-interaction-type http

--- tests/test-docker-get-code.sh
+++ tests/test-docker-get-code.sh
@@ -17,7 +17,7 @@ export MWCLI_CONTEXT_TEST=1
 TEST_DIR=$(mktemp -d)
 TEST_TIMESTAMP=$(date +%s)
 test_command_success ./bin/mw docker --context test-${TEST_TIMESTAMP}-just-core env set MEDIAWIKI_VOLUMES_CODE ${TEST_DIR}
-test_command_success ./bin/mw docker --context test-${TEST_TIMESTAMP}-just-core mediawiki get-code --no-interaction --core --shallow --use-github
+test_command_success ./bin/mw docker --context test-${TEST_TIMESTAMP}-just-core mediawiki get-code --no-interaction --core --shallow --use-github --gerrit-interaction-type http
 test_command_success cat ${TEST_DIR}/index.php
 test_command sh -c "ls -lahrt ${TEST_DIR}/extensions | grep drwx | wc -l" 2
 test_command sh -c "ls -lahrt ${TEST_DIR}/skins | grep drwx | wc -l" 2
@@ -25,7 +25,7 @@ test_command sh -c "ls -lahrt ${TEST_DIR}/skins | grep drwx | wc -l" 2
 # Check we can choose a few skins and extensions too
 TEST_DIR=$(mktemp -d)
 test_command_success ./bin/mw docker --context test-${TEST_TIMESTAMP}-core-plus env set MEDIAWIKI_VOLUMES_CODE ${TEST_DIR}
-test_command_success ./bin/mw docker --context test-${TEST_TIMESTAMP}-core-plus mediawiki get-code --no-interaction --core --skin Vector --skin Timeless --extension Nuke --extension Cognate --shallow
+test_command_success ./bin/mw docker --context test-${TEST_TIMESTAMP}-core-plus mediawiki get-code --no-interaction --core --skin Vector --skin Timeless --extension Nuke --extension Cognate --shallow --gerrit-interaction-type http
 test_command_success cat ${TEST_DIR}/index.php
 test_command sh -c "ls -lahrt ${TEST_DIR}/extensions | grep drwx | wc -l" 4
 test_command ls -lahrt ${TEST_DIR}/extensions "Nuke"
