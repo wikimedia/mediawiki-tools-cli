b7a79f50d0c1010fe3282badbccc4c039f96a20e
addshore <addshorewiki@gmail.com>
2024-05-06 15:11:17 +0100

    Show multiple changelogs on update

--- internal/cmd/update/update.go
+++ internal/cmd/update/update.go
@@ -3,6 +3,7 @@ package update
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/AlecAivazis/survey/v2"
@@ -13,7 +14,7 @@ import (
 )
 
 func NewUpdateCmd() *cobra.Command {
-	manualVersion := ""
+	versionInput := ""
 	cmd := &cobra.Command{
 		Use:   "update",
 		Short: "Checks for and performs updates",
@@ -21,9 +22,13 @@ func NewUpdateCmd() *cobra.Command {
 update --version=0.10 --no-interaction
 update --version=https://gitlab.wikimedia.org/repos/releng/cli/-/jobs/252738/artifacts/download`,
 		Run: func(cmd *cobra.Command, args []string) {
+			currentVersion := cli.VersionDetails.Version
+			var targetVersion string
+			var targetArtifact string
+
 			// No manual version, so genrally check for new releases
-			if manualVersion == "" {
-				canUpdate, toUpdateToOrMessage := updater.CanUpdate(cli.VersionDetails.Version, cli.VersionDetails.GitSummary)
+			if versionInput == "" {
+				canUpdate, toUpdateToOrMessage := updater.CanUpdate(currentVersion, cli.VersionDetails.GitSummary)
 
 				if !canUpdate {
 					fmt.Println(toUpdateToOrMessage)
@@ -31,23 +36,27 @@ update --version=https://gitlab.wikimedia.org/repos/releng/cli/-/jobs/252738/art
 				}
 
 				fmt.Println("New update found: " + toUpdateToOrMessage)
+				targetVersion = toUpdateToOrMessage
 			}
 
 			// Manual version is specified, so check it
-			if manualVersion != "" {
+			if versionInput != "" {
 				// if manual version looks like a URL, we will just try and download it later
-				if manualVersion[:4] == "http" {
-					fmt.Println("Downloading from URL: " + manualVersion)
+				if versionInput[:4] == "http" {
+					fmt.Println("Downloading from URL: " + versionInput)
+					targetArtifact = versionInput
 				} else {
-					canMoveToVersion := updater.CanMoveToVersion(manualVersion)
+					canMoveToVersion := updater.CanMoveToVersion(versionInput)
 					if !canMoveToVersion {
-						fmt.Println("Can not find manual version " + manualVersion + " to move to")
+						fmt.Println("Can not find manual version " + versionInput + " to move to")
 						os.Exit(1)
 					}
-					fmt.Println("Updating to manually selected version: " + manualVersion)
+					fmt.Println("Updating to manually selected version: " + versionInput)
+					targetVersion = versionInput
 				}
 			}
 
+			// If we are in interactive mode, confirm the user wants to continue with the update
 			if !cli.Opts.NoInteraction {
 				response := false
 				prompt := &survey.Confirm{
@@ -66,7 +75,7 @@ update --version=https://gitlab.wikimedia.org/repos/releng/cli/-/jobs/252738/art
 
 			// Start a progress bar
 			updateProcessCompleted := false
-			bar := progressbar.Default(100, "Updating binary")
+			bar := progressbar.Default(111, "Updating binary")
 			go func() {
 				for !updateProcessCompleted {
 					err := bar.Add(1)
@@ -79,68 +88,65 @@ update --version=https://gitlab.wikimedia.org/repos/releng/cli/-/jobs/252738/art
 
 			// Perform the update
 			var updateSuccess bool
-			var updateMessage string
-			if manualVersion == "" {
-				// Technically there is a small race condition here, and we might update to a newer version if it was release between stages
-				updateSuccess, updateMessage = updater.Update(cli.VersionDetails.Version, cli.VersionDetails.GitSummary)
-			} else {
-				if manualVersion[:4] == "http" {
-					tempDownloadFile, err := updater.DownloadFile(manualVersion)
-					if err != nil {
-						fmt.Println(err)
-						os.Exit(1)
-					}
-					// Extract tempFile which is a zip file
-					tempDir, err := os.MkdirTemp("", "mwcli-update")
-					if err != nil {
-						fmt.Println(err)
-						os.Exit(1)
-					}
-					defer os.RemoveAll(tempDir)
+			// Either from a Gitlab release
+			if targetVersion != "" {
+				updateSuccess, _ = updater.MoveToVersion(targetVersion)
+			}
+			// For from a Gitlab build artifact
+			if targetArtifact != "" {
+				tempDownloadFile, err := updater.DownloadFile(targetArtifact)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+				// Extract tempFile which is a zip file
+				tempDir, err := os.MkdirTemp("", "mwcli-update")
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+				defer os.RemoveAll(tempDir)
 
-					err = updater.Unzip(tempDownloadFile, tempDir)
-					if err != nil {
-						fmt.Println("Could not unzip the downloaded file: " + tempDownloadFile)
-						os.Exit(1)
-					}
-					// it should contain a dir called bin, and in that a file called mw
-					newMwFileLocation := tempDir + "/bin/mw"
+				err = updater.Unzip(tempDownloadFile, tempDir)
+				if err != nil {
+					fmt.Println("Could not unzip the downloaded file: " + tempDownloadFile)
+					os.Exit(1)
+				}
+				// it should contain a dir called bin, and in that a file called mw
+				newMwFileLocation := tempDir + "/bin/mw"
 
-					// Make sure it exists or error
-					if _, err := os.Stat(newMwFileLocation); os.IsNotExist(err) {
-						fmt.Println("Could not find the mw binary in the downloaded file: " + newMwFileLocation)
-						os.Exit(1)
-					}
+				// Make sure it exists or error
+				if _, err := os.Stat(newMwFileLocation); os.IsNotExist(err) {
+					fmt.Println("Could not find the mw binary in the downloaded file: " + newMwFileLocation)
+					os.Exit(1)
+				}
 
-					// Move the current bin to a temp location
-					oldFileName := os.Args[0] + ".old." + time.Now().Format("2006-01-02-15-04-05")
-					err = os.Rename(os.Args[0], oldFileName)
-					if err != nil {
-						fmt.Println(err)
-						os.Exit(1)
-					}
-					// defer deletion of this file
-					defer os.Remove(oldFileName)
+				// Move the current bin to a temp location
+				oldFileName := os.Args[0] + ".old." + time.Now().Format("2006-01-02-15-04-05")
+				err = os.Rename(os.Args[0], oldFileName)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+				// defer deletion of this file
+				defer os.Remove(oldFileName)
 
-					// Move the new file to the current location
-					err = os.Rename(newMwFileLocation, os.Args[0])
-					if err != nil {
-						// Switch them back
-						os.Rename(oldFileName, os.Args[0])
-						fmt.Println(err)
-						os.Exit(1)
-					}
+				// Move the new file to the current location
+				err = os.Rename(newMwFileLocation, os.Args[0])
+				if err != nil {
+					// Switch them back
+					os.Rename(oldFileName, os.Args[0])
+					fmt.Println(err)
+					os.Exit(1)
+				}
 
-					// Make sure it is executable
-					err = os.Chmod(os.Args[0], 0o755)
-					if err != nil {
-						// Switch them back
-						os.Rename(oldFileName, os.Args[0])
-						fmt.Println(err)
-						os.Exit(1)
-					}
-				} else {
-					updateSuccess, updateMessage = updater.MoveToVersion(manualVersion)
+				// Make sure it is executable
+				err = os.Chmod(os.Args[0], 0o755)
+				if err != nil {
+					// Switch them back
+					os.Rename(oldFileName, os.Args[0])
+					fmt.Println(err)
+					os.Exit(1)
 				}
 			}
 
@@ -151,17 +157,35 @@ update --version=https://gitlab.wikimedia.org/repos/releng/cli/-/jobs/252738/art
 				fmt.Println(err)
 			}
 
-			// Output result, if we are moving to a real release
-			if manualVersion[:4] != "http" {
-				fmt.Println("")
-				fmt.Println(cli.RenderMarkdown(updateMessage))
-			}
-
+			// Exit with 1 if we didn't update
 			if !updateSuccess {
 				os.Exit(1)
 			}
+
+			// Figure out what to tell the user about the update
+
+			// Output changelog of the versions we are moving between
+			if targetVersion != "" {
+				releasesUpdatedThrough, err := updater.RelengCliGetReleasesBetweenTags(currentVersion, targetVersion)
+				if err != nil {
+					fmt.Printf("Could not fetch changelog between versions: %s\n", err)
+					// TODO link to releases page
+				} else {
+					fmt.Print("\nChanges between versions:\n\n")
+					for _, release := range releasesUpdatedThrough {
+						desc := strings.Trim(release.Description, "\r\n")
+						// TODO Remove any lines that start with "CHANGELOG extracted from"
+						formatted := strings.Trim(cli.RenderMarkdown(desc), "\r\n")
+						fmt.Print(formatted)
+					}
+				}
+			}
+			if targetArtifact != "" {
+				fmt.Println("Updated from a build artifact")
+				// TODO link
+			}
 		},
 	}
-	cmd.Flags().StringVarP(&manualVersion, "version", "", "", "Specific version to \"update\" to, or rollback to.")
+	cmd.Flags().StringVarP(&versionInput, "version", "", "", "Specific version to \"update\" to, or rollback to.")
 	return cmd
 }
