ef15524f6d48acfbd24eb057c2ef5c9c70d3f092
addshore <addshorewiki@gmail.com>
2025-01-25 21:38:04 +0000

    DOCKER_DEFAULT_PLATFORM=linux/amd64 on linux arm
    Bug: T355341

--- pkg/dockercompose/command.go
+++ pkg/dockercompose/command.go
@@ -21,15 +21,38 @@ func (c Command) RunAttached() error {
 	c.Cmd.Stdout = os.Stdout
 	c.Cmd.Stdin = os.Stdin
 	c.Cmd.Stderr = os.Stderr
-	c.logRun()
-	err := c.Cmd.Run()
+	err := c.run()
 	return err
 }
 
 func (c Command) RunAndCollect() (stdout bytes.Buffer, stderr bytes.Buffer, err error) {
 	c.Cmd.Stdout = &stdout
 	c.Cmd.Stderr = &stderr
-	c.logRun()
-	err = c.Cmd.Run()
+	err = c.run()
 	return stdout, stderr, err
+}
+
+func (c Command) run() error {
+	c.logRun()
+	if isLinux() && isArm() && !isDockerDefaultPlatformDefined() {
+		// If we are a linux arm machine, we need to force the default platform to linux/amd64
+		// As we don't have arm images for all services https://phabricator.wikimedia.org/T355341
+		logrus.Trace("Forcing DOCKER_DEFAULT_PLATFORM to linux/amd64")
+		c.Cmd.Env = append(c.Cmd.Env, "DOCKER_DEFAULT_PLATFORM=linux/amd64")
+	}
+	return c.Cmd.Run()
+}
+
+func isDockerDefaultPlatformDefined() bool {
+	_, ok := os.LookupEnv("DOCKER_DEFAULT_PLATFORM")
+	return ok
+}
+
+func isLinux() bool {
+	return os.Getenv("GOOS") == "linux"
+}
+
+func isArm() bool {
+	arch := os.Getenv("GOARCH")
+	return arch == "arm" || arch == "arm64"
 }
