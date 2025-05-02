wikimedia/mediawiki-tools-cli

commit b4eeddffdc789224b8df11afd6dabfb0b635ef95

Detect invalid subcommands and error
Bug: T293062

cmd/root.go
@@ -86,6 +86,17 @@ func NewMwCliCmd() *cobra.Command {
 
 	defaultHelpFunc := mwcliCmd.HelpFunc()
 	mwcliCmd.SetHelpFunc(func(c *cobra.Command, a []string) {
+		// Check that the command being run is actually a known command (or alias)
+		// Otherwise we want to output an error saying "unknown command", and do a non 0 exit
+		// See https://github.com/spf13/cobra/issues/706
+		mwa := "mw " + strings.Join(a, " ")
+		if !strings.Contains(mwa, "--help") && !stringsutil.StringInSlice(mwa, cobrautil.AllFullCommandStringsFromParent(mwcliCmd)) {
+			logrus.Errorf("unknown command: %s", strings.Join(a, " "))
+			c.Root().Annotations = make(map[string]string)
+			c.Root().Annotations["exitCode"] = "1"
+			return
+		}
+
 		eventlogging.AddCommandRunEvent(strings.Trim(cobrautil.FullCommandStringWithoutPrefix(c, "mw")+" --help", " "), cli.VersionDetails.Version)
 		defaultHelpFunc(c, a)
 	})

internal/cmd/docker/docker.go
@@ -101,7 +101,7 @@ func NewCmd() *cobra.Command {
 			thisDev.EnsureReady()
 
 			// Skip the checks and wizard for some sub commands
-			if cobrautil.CommandIsSubCommandOfOneOrMore(cmd, ignoreMwddPersistentRunForPrefixes) {
+			if cobrautil.CommandIsSubCommandOfOneOrMoreStrings(cmd, ignoreMwddPersistentRunForPrefixes) {
 				return
 			}
 			// Skip the checks and wizard for any destroy commands

.golangci.yml
@@ -12,7 +12,6 @@ linters:
     - bodyclose
     - depguard
     - gci
-    - godot
     - gofmt
     - gofumpt
     - goimports

internal/cmd/gitlab/gitlab.go
@@ -37,7 +37,7 @@ func NewGitlabCmd() *cobra.Command {
 	})
 
 	// Remove all "v" shorthands for command flags recursively
-	cobrautil.VisitAllCommands(glabCommand, func(cmd *cobra.Command) {
+	cobrautil.VisitAllSubCommands(glabCommand, func(cmd *cobra.Command) {
 		originalFlags := cmd.Flags()
 		if originalFlags.ShorthandLookup("v") != nil {
 			cmd.ResetFlags()

internal/util/cobra/command.go
@@ -16,32 +16,83 @@ func FullCommandString(cmd *cobra.Command) string {
 	return strings.Trim(s, " ")
 }
 
+// FullCommandStrings returns a list of all full command strings for a given command.
+// This includes variations with aliases at all levels.
+func FullCommandStrings(cmd *cobra.Command) []string {
+	strings := []string{}
+	strings = append(strings, cmd.Name())
+	for _, alias := range cmd.Aliases {
+		strings = append(strings, alias)
+	}
+
+	for cmd.HasParent() {
+		thisLevelStrings := []string{}
+		thisLevelStrings = append(thisLevelStrings, cmd.Parent().Name())
+		for _, alias := range cmd.Parent().Aliases {
+			thisLevelStrings = append(thisLevelStrings, alias)
+		}
+
+		// For each this level string, multiply out the strings from the next level, so we have all combinations
+		newStrings := []string{}
+		for _, thisLevelString := range thisLevelStrings {
+			for _, s := range strings {
+				newStrings = append(newStrings, thisLevelString+" "+s)
+			}
+		}
+		strings = newStrings
+
+		cmd = cmd.Parent()
+	}
+
+	return strings
+}
+
 // FullCommandStringWithoutPrefix removes an optional prefix from FullCommandString
 // This can be used to, for example, remove the root command from the string with ease.
 func FullCommandStringWithoutPrefix(cmd *cobra.Command, prefix string) string {
 	return strings.Trim(strings.TrimPrefix(FullCommandString(cmd), prefix), " ")
 }
 
-// CommandIsSubCommandOf detects if a command is a subcommand (or command) or a given command string
-// Example: CommandIsSubCommandOf(cmd, "mw docker env") returns true if cmd is a subcommand of docker env command
-// Canonical names must be used, aliases will not work.
-func CommandIsSubCommandOf(cmd *cobra.Command, subCommandString string) bool {
-	fullCommandString := FullCommandString(cmd)
-	return strings.HasPrefix(fullCommandString, subCommandString)
+// CommandIsSubCommandOfString detects if a command is a subcommand of a given command string.
+// Example: CommandIsSubCommandOfString(cmd, "mw docker env") returns true if cmd is a subcommand of docker env command.
+func CommandIsSubCommandOfString(cmd *cobra.Command, cmdString string) bool {
+	fullCommandStrings := FullCommandStrings(cmd)
+
+	for _, fullCommandString := range fullCommandStrings {
+		if strings.HasPrefix(fullCommandString, cmdString) {
+			return true
+		}
+	}
+
+	return false
 }
 
-func CommandIsSubCommandOfOneOrMore(cmd *cobra.Command, subCommandStrings []string) bool {
+// Same as CommandIsSubCommandOfString but for multiple strings
+func CommandIsSubCommandOfOneOrMoreStrings(cmd *cobra.Command, subCommandStrings []string) bool {
 	for _, subCommandString := range subCommandStrings {
-		if CommandIsSubCommandOf(cmd, subCommandString) {
+		if CommandIsSubCommandOfString(cmd, subCommandString) {
 			return true
 		}
 	}
 	return false
 }
 
-func VisitAllCommands(cmd *cobra.Command, fn func(*cobra.Command)) {
+// VisitAllCommands visits all commands and subcommands of a given command
+// It can be used to perform alterations on a tree of commands that perhaps you havn't created yourself.
+func VisitAllSubCommands(cmd *cobra.Command, fn func(*cobra.Command)) {
 	fn(cmd)
 	for _, subCmd := range cmd.Commands() {
-		VisitAllCommands(subCmd, fn)
+		VisitAllSubCommands(subCmd, fn)
 	}
+}
+
+// AllFullCommandStringsFromParent returns all full command strings for all subcommands of a given command
+// This includes variations with aliases at all levels
+// This can be used to check if a command is a subcommand of a given cmd
+func AllFullCommandStringsFromParent(cmd *cobra.Command) []string {
+	strings := []string{}
+	VisitAllSubCommands(cmd, func(c *cobra.Command) {
+		strings = append(strings, FullCommandStrings(c)...)
+	})
+	return strings
 }

internal/util/cobra/command_test.go
@@ -101,7 +101,7 @@ func TestFullCommandStringWithoutPrefix(t *testing.T) {
 	}
 }
 
-func TestCommandIsSubCommandOf(t *testing.T) {
+func TestCommandIsSubCommandOfString(t *testing.T) {
 	_, _, lv3Cmd := threeFakeCmds()
 
 	type args struct {
@@ -140,14 +140,14 @@ func TestCommandIsSubCommandOf(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := CommandIsSubCommandOf(tt.args.cmd, tt.args.subCommand); got != tt.want {
+			if got := CommandIsSubCommandOfString(tt.args.cmd, tt.args.subCommand); got != tt.want {
 				t.Errorf("CommandIsSubCommandOf() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func TestCommandIsSubCommandOfOneOrMore(t *testing.T) {
+func TestCommandIsSubCommandOfOneOrMoreStrings(t *testing.T) {
 	_, _, lv3Cmd := threeFakeCmds()
 
 	type args struct {
@@ -194,8 +194,109 @@ func TestCommandIsSubCommandOfOneOrMore(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := CommandIsSubCommandOfOneOrMore(tt.args.cmd, tt.args.subCommands); got != tt.want {
+			if got := CommandIsSubCommandOfOneOrMoreStrings(tt.args.cmd, tt.args.subCommands); got != tt.want {
 				t.Errorf("CommandIsSubCommandOfOneOrMore() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFullCommandStrings(t *testing.T) {
+	lv1Cmd := &cobra.Command{
+		Use:     "level1",
+		Aliases: []string{"l1", "one"},
+	}
+	lv2Cmd := &cobra.Command{
+		Use:     "level2",
+		Aliases: []string{"l2", "two"},
+	}
+	lv1Cmd.AddCommand(lv2Cmd)
+	lv3Cmd := &cobra.Command{
+		Use:     "level3",
+		Aliases: []string{"l3", "three"},
+	}
+	lv2Cmd.AddCommand(lv3Cmd)
+
+	tests := []struct {
+		name string
+		cmd  *cobra.Command
+		want []string
+	}{
+		{
+			name: "level 1 command with aliases",
+			cmd:  lv1Cmd,
+			want: []string{"level1", "l1", "one"},
+		},
+		{
+			name: "level 2 command with aliases",
+			cmd:  lv2Cmd,
+			want: []string{
+				"level1 level2",
+				"level1 l2",
+				"level1 two",
+				"l1 level2",
+				"l1 l2",
+				"l1 two",
+				"one level2",
+				"one l2",
+				"one two",
+			},
+		},
+		{
+			name: "level 3 command with aliases",
+			cmd:  lv3Cmd,
+			want: []string{
+				"level1 level2 level3",
+				"level1 level2 l3",
+				"level1 level2 three",
+				"level1 l2 level3",
+				"level1 l2 l3",
+				"level1 l2 three",
+				"level1 two level3",
+				"level1 two l3",
+				"level1 two three",
+				"l1 level2 level3",
+				"l1 level2 l3",
+				"l1 level2 three",
+				"l1 l2 level3",
+				"l1 l2 l3",
+				"l1 l2 three",
+				"l1 two level3",
+				"l1 two l3",
+				"l1 two three",
+				"one level2 level3",
+				"one level2 l3",
+				"one level2 three",
+				"one l2 level3",
+				"one l2 l3",
+				"one l2 three",
+				"one two level3",
+				"one two l3",
+				"one two three",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := FullCommandStrings(tt.cmd)
+			gotMap := make(map[string]bool)
+			wantMap := make(map[string]bool)
+			for _, g := range got {
+				gotMap[g] = true
+			}
+			for _, w := range tt.want {
+				wantMap[w] = true
+			}
+			if len(gotMap) != len(wantMap) {
+				t.Errorf("FullCommandStrings() = %v, want %v", got, tt.want)
+				return
+			}
+			for w := range wantMap {
+				if !gotMap[w] {
+					t.Errorf("FullCommandStrings() = %v, want %v", got, tt.want)
+					break
+				}
 			}
 		})
 	}

internal/util/strings/replace.go
@@ -1,0 +1,11 @@
+package strings
+
+import "strings"
+
+func ReplaceLastOccurrence(s, find, replace string) string {
+	lastIndex := strings.LastIndex(s, find)
+	if lastIndex == -1 {
+		return s
+	}
+	return s[:lastIndex] + replace + s[lastIndex+len(find):]
+}

commit c8917d0a3e31e721a13ef1665784bcd2da23c854

Allow global flags again in docker command
I believe this was broken in b4eeddffdc789224b8df11afd6dabfb0b635ef95
Around 0.26.+? maybe
Now i just throw the flags away based on them starting with a - ....

cmd/root.go
@@ -83,12 +83,16 @@ func NewMwCliCmd() *cobra.Command {
 
 	defaultHelpFunc := mwcliCmd.HelpFunc()
 	mwcliCmd.SetHelpFunc(func(c *cobra.Command, a []string) {
-		// Check that the command being run is actually a known command (or alias)
-		// Otherwise we want to output an error saying "unknown command", and do a non 0 exit
-		// See https://github.com/spf13/cobra/issues/706
-		mwa := "mw " + strings.Join(a, " ")
-		if len(a) != 0 && !strings.Contains(mwa, "--help") && !stringsutil.StringInSlice(mwa, cobrautil.AllFullCommandStringsFromParent(mwcliCmd)) {
-			logrus.Errorf("unknown command: %s", strings.Join(a, " "))
+		// Remove flags (arguments starting with -) from a, so we only check actual command words
+		commandWords := []string{}
+		for _, arg := range a {
+			if !strings.HasPrefix(arg, "-") {
+				commandWords = append(commandWords, arg)
+			}
+		}
+		mwa := "mw " + strings.Join(commandWords, " ")
+		if len(commandWords) != 0 && !strings.Contains(mwa, "--help") && !stringsutil.StringInSlice(mwa, cobrautil.AllFullCommandStringsFromParent(mwcliCmd)) {
+			logrus.Errorf("unknown command: %s", strings.Join(commandWords, " "))
 			c.Root().Annotations = make(map[string]string)
 			c.Root().Annotations["exitCode"] = "1"
 			return

internal/cmd/docker/docker.go
@@ -168,7 +168,7 @@ func NewCmd() *cobra.Command {
 	// Parse PersistentFlags early so that the context is already known to other commands that are added
 	err := cmd.PersistentFlags().Parse(os.Args[1:])
 	if err != nil {
-		logrus.Tracef("Error parsing persistent flags: %s", err)
+		logrus.Tracef("Error parsing persistent flags in docker command: %s", err)
 	}
 
 	if cli.MwddIsDevAlias {

tests/test-docker-general.sh
@@ -32,6 +32,10 @@ trap finish EXIT
 
 test_command_success ./bin/mw docker env clear --no-interaction
 
+# Also test using and clearing a different context...
+
+test_command_success ./bin/mw docker env clear --no-interaction --context=anotherone
+
 # Run this integration test using a non standard port, unlikley to conflict, to make sure it works
 test_command_success ./bin/mw docker env set PORT 6194
 # And already fill in the location of mediawiki