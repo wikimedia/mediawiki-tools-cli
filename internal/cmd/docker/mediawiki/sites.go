9145a060b1af55dd506ffc79514e347002e623ca
addshore <addshorewiki@gmail.com>
2025-05-03 18:21:18 +0300

    dev: Adds a sites name to the data
    Bug: T393267

--- internal/cmd/docker/mediawiki/sites.go
+++ internal/cmd/docker/mediawiki/sites.go
@@ -6,28 +6,31 @@ import (
 
 	"github.com/spf13/cobra"
 	"gitlab.wikimedia.org/repos/releng/cli/internal/mwdd"
+	cobrautil "gitlab.wikimedia.org/repos/releng/cli/internal/util/cobra"
 	"gitlab.wikimedia.org/repos/releng/cli/internal/util/output"
 )
 
 func NewMediaWikiSitesCmd() *cobra.Command {
 	type Site struct {
+		Name string
 		Host string
 		URL  string
 	}
 	out := output.Output{
 		TopLevelKeys: false,
 		TableBinding: &output.TableBinding{
-			Headings: []string{"Host", "URL"},
+			Headings: []string{"Name", "Host", "URL"},
 			ProcessObjects: func(objects map[interface{}]interface{}, table *output.Table) {
 				for _, object := range objects {
 					typedObject := object.(Site)
-					table.AddRowS(typedObject.Host, typedObject.URL)
+					table.AddRowS(typedObject.Name, typedObject.Host, typedObject.URL)
 				}
 			},
 		},
 		AckBinding: func(objects map[interface{}]interface{}, ack *output.Ack) {
 			for _, object := range objects {
 				typedObject := object.(Site)
+				ack.AddItem(typedObject.Host, fmt.Sprintf("Name: %s", typedObject.Name))
 				ack.AddItem(typedObject.Host, fmt.Sprintf("Host: %s", typedObject.Host))
 				ack.AddItem(typedObject.Host, fmt.Sprintf("URL: %s", typedObject.URL))
 			}
@@ -36,6 +39,10 @@ func NewMediaWikiSitesCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "sites",
 		Short: "Lists sites created in your environment (since the last top level destroy command was run)",
+		Example: cobrautil.NormalizeExample(`
+		sites
+		sites --output json --format .Name | jq -r
+	`),
 		Run: func(cmd *cobra.Command, args []string) {
 			mwdd.DefaultForUser().EnsureReady()
 
@@ -43,7 +50,10 @@ func NewMediaWikiSitesCmd() *cobra.Command {
 			objects := make(map[interface{}]interface{})
 			for _, host := range mwdd.DefaultForUser().UsedHosts() {
 				if strings.Contains(host, "mediawiki.mwdd") {
+					// Make a horrible assumption that the bit before the first dot is the site name
+					name := strings.Split(host, ".")[0]
 					objects[host] = Site{
+						Name: name,
 						Host: host,
 						URL:  fmt.Sprintf("http://%s:%s", host, mwdd.DefaultForUser().Env().Get("PORT")),
 					}
