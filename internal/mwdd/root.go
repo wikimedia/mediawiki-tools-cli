a0e810edaa9211ac5e59cf4bc8001c49b4f8d5a8
Mstyles <mstyles@wikimedia.org>
2025-05-06 10:23:37 +0000

    Refactor internal/mw and internal/mwdd
    * fix bug
    
    * refactor internal/mw and internal/mwdd

--- internal/mwdd/root.go
+++ internal/mwdd/root.go
@@ -4,7 +4,6 @@ import (
 	"os"
 	"regexp"
 
-	"github.com/sirupsen/logrus"
 	"gitlab.wikimedia.org/repos/releng/cli/internal/cli"
 	"gitlab.wikimedia.org/repos/releng/cli/internal/mwdd/files"
 	"gitlab.wikimedia.org/repos/releng/cli/pkg/dockercompose"
@@ -37,57 +36,6 @@ func (m MWDD) Directory() string {
 func (m MWDD) EnsureReady() {
 	files.EnsureReady(m.Directory())
 	m.Env().EnsureExists()
-}
-
-func (m MWDD) ComposerLocalJsonPath() string {
-	// TODO move to internal.mediawiki
-	return m.Env().Get("MEDIAWIKI_VOLUMES_CODE") + string(os.PathSeparator) + "composer.local.json"
-}
-
-func (m MWDD) ComposerLocalJsonExists() bool {
-	// TODO move to internal.mediawiki
-	_, err := os.Stat(m.ComposerLocalJsonPath())
-	return !os.IsNotExist(err)
-}
-
-func (m MWDD) LocalSettingsPath() string {
-	// TODO move to internal.mediawiki
-	return m.Env().Get("MEDIAWIKI_VOLUMES_CODE") + string(os.PathSeparator) + "LocalSettings.php"
-}
-
-func (m MWDD) LocalSettingsContents() string {
-	// TODO move to internal.mediawiki
-	bytes, err := os.ReadFile(m.LocalSettingsPath())
-	if err != nil {
-		logrus.Fatal(err)
-		os.Exit(1)
-	}
-	return string(bytes)
-}
-
-func (m MWDD) ExtensionsCheckedOut() []string {
-	// TODO move to internal.mediawiki
-	return directoriesInDirectory(m.Env().Get("MEDIAWIKI_VOLUMES_CODE") + string(os.PathSeparator) + "extensions")
-}
-
-func (m MWDD) SkinsCheckedOut() []string {
-	// TODO move to internal.mediawiki
-	return directoriesInDirectory(m.Env().Get("MEDIAWIKI_VOLUMES_CODE") + string(os.PathSeparator) + "skins")
-}
-
-func directoriesInDirectory(directory string) []string {
-	entries, err := os.ReadDir(directory)
-	if err != nil {
-		logrus.Fatal(err)
-		os.Exit(1)
-	}
-	directories := []string{}
-	for _, e := range entries {
-		if e.IsDir() {
-			directories = append(directories, e.Name())
-		}
-	}
-	return directories
 }
 
 func (m MWDD) DockerCompose() dockercompose.Project {
