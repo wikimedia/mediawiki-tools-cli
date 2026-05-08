8a3c3957516e560d6be37f890a819006cd4e0274
addshore <git@addshore.com>
2026-05-08 20:21:18 +0100

    Fix template and example

--- internal/util/output/template.go
+++ internal/util/output/template.go
@@ -3,6 +3,7 @@ package output
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"text/template"
 
@@ -19,7 +20,7 @@ func printTemplate(objects interface{}, format string, writer io.Writer) {
 
 	// Normalise via JSON round-trip so template can access fields uniformly
 	var data interface{}
-	b, err := json.Marshal(objects)
+	b, err := json.Marshal(normalizeForTemplate(objects))
 	if err != nil {
 		logrus.Errorf("template: marshal error: %v", err)
 		return
@@ -42,5 +43,32 @@ func printTemplate(objects interface{}, format string, writer io.Writer) {
 	}
 	if _, err := io.Copy(writer, &buf); err != nil {
 		logrus.Errorf("template: write error: %v", err)
+	}
+}
+
+// normalizeForTemplate recursively converts maps with non-string keys into
+// map[string]interface{} so they can be marshaled to JSON.
+func normalizeForTemplate(in interface{}) interface{} {
+	switch typed := in.(type) {
+	case map[interface{}]interface{}:
+		out := make(map[string]interface{}, len(typed))
+		for key, value := range typed {
+			out[fmt.Sprintf("%v", key)] = normalizeForTemplate(value)
+		}
+		return out
+	case map[string]interface{}:
+		out := make(map[string]interface{}, len(typed))
+		for key, value := range typed {
+			out[key] = normalizeForTemplate(value)
+		}
+		return out
+	case []interface{}:
+		out := make([]interface{}, len(typed))
+		for i, value := range typed {
+			out[i] = normalizeForTemplate(value)
+		}
+		return out
+	default:
+		return in
 	}
 }
